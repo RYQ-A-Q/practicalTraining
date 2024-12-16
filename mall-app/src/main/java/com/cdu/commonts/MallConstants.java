3d63c871c1c647a538b7a4e27091f58942cc885f
RYQ-A-Q <r2649869344@outlook.com>
2024-12-16 16:50:05 +0800

    - 在 MallConstants 类中添加 IS_NOT_DELETE 和 IS_DELETE常量 - 分别表示未删除和删除状态

--- mall-app/src/main/java/com/cdu/commonts/MallConstants.java
+++ mall-app/src/main/java/com/cdu/commonts/MallConstants.java
@@ -3,4 +3,8 @@ package com.cdu.commonts;
 public class MallConstants {
     /**散列次数 */
     public static final int HASH_TIME=5;
+    //未删除
+    public final static int IS_NOT_DELETE = 0;
+    //删除
+    public final static int IS_DELETE = 1;
 }
