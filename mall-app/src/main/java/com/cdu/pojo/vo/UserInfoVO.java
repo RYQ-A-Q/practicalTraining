74e87f643fe70428055a15dd04373ff4e53423d6
RYQ-A-Q <r2649869344@outlook.com>
2024-12-17 16:02:05 +0800

    feat: 添加 UserInfoVO 类
    - 新增 UserInfoVO 类用于个人资料展示
    - 包含用户名、电话、邮箱、性别等属性

---
+++ mall-app/src/main/java/com/cdu/pojo/vo/UserInfoVO.java
@@ -0,0 +1,14 @@
+package com.cdu.pojo.vo;
+
+import lombok.Data;
+
+/**
+ *个人资料VO
+ */
+@Data
+public class UserInfoVO {
+   private String username;
+   private String phone;
+   private String email;
+   private String gender;
+}
